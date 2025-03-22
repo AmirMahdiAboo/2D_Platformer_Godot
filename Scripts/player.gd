20ea6f38b639d8eca0983908865b299b95d4b83f
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2025-03-23 03:09:22 +0330

    player jump key issue resolved

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 const SPEED = 150.0
@@ -11,15 +12,16 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.scale.x = -1 if direction < 0 else 1  # Flip sprite when moving left/right
+		animated_sprite_2d.scale.x = -1 if direction < 0 else 1  # Flip sprite when moving left/right
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
